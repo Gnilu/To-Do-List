af26f461705830b616e430968c17bffc6e7866ab
Gnilu <gayani2000nilushi@gmail.com>
2024-07-24 12:02:31 -0700

    frontend creating

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function Create() {
+  return (
+    <div>
+        <input type='text' name='' id='' placeholder='Enter task' />
+        <button type='button'>Add</button>
+    </div>
+  )
+}
+
+export default Create
---
+++ todolist/src/Home.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import Create from './Create'
+import './App.css'
+
+function Home() {
+    const [todos, setTodos] = useState([])
+  return (
+    <div className='home'>
+        <h2>Todo List</h2>
+        <Create />
+        {
+            todos.length === 0 
+            ?
+            <div><h2>No Record</h2></div>
+            :
+            todos.map((todo,index) => (
+                <div key={index}>
+                    {todo}
+                </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Home
